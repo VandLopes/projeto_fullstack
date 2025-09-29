a20f85bafed97abc37d667210b510df962765f42
VandLopes <vanderleilopes23.vl@gmail.com>
2025-09-28 23:22:04 -0300

    finalizando projeto full stack, criados os metodos no js para criar os elementos html na tela e carregar os dados do banco de dados

---
+++ frontend/js/script.js
@@ -0,0 +1,154 @@
+const tbody = document.querySelector("tbody");
+const addForm = document.querySelector(".add-form");
+const inputTask = document.querySelector(".input-task");
+
+const fetchTasks = async () => {
+  const response = await fetch("http://localhost:3000/tasks");
+  const tasks = await response.json();
+  return tasks;
+};
+
+const addTask = async (event) => {
+  event.preventDefault();
+
+  const task = { title: inputTask.value };
+
+  await fetch("http://localhost:3000/tasks", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  });
+
+  loadTask();
+  inputTask.value = "";
+};
+
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:3000/tasks/${id}`, {
+    method: "delete",
+  });
+
+  loadTask();
+};
+
+const updateTask = async ({ id, title, status }) => {
+  await fetch(`http://localhost:3000/tasks/${id}`, {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, status }),
+  });
+
+  loadTask();
+};
+
+const formatDate = (dateUTC) => {
+  const options = { dateStyle: "long", timeStyle: "short" };
+  const date = new Date(dateUTC).toLocaleString("pt-br", options);
+  return date;
+};
+
+const createElement = (tag, innerText = "", innerHTML = "") => {
+  const element = document.createElement(tag);
+
+  if (innerText) {
+    element.innerText = innerText;
+  }
+
+  if (innerHTML) {
+    element.innerHTML = innerHTML;
+  }
+
+  return element;
+};
+
+const createSelect = (value) => {
+  const options = `
+      <option value="pendente">pendente</option>
+      <option value="em andamento">em andamento</option>
+      <option value="concluida">concluida</option>
+      `;
+
+  const select = createElement("select", "", options);
+
+  select.value = value;
+
+  return select;
+};
+
+const createRow = (task) => {
+  const { id, title, created_at, status } = task;
+
+  const tr = createElement("tr");
+  const tdTitle = createElement("td", title);
+  const tdCreatedAt = createElement("td", formatDate(created_at));
+  const tdStatus = createElement("td");
+  const tdAction = createElement("td");
+
+  const select = createSelect(status);
+
+  select.addEventListener("change", ({ target }) =>
+    updateTask({ ...task, id, title, created_at, status: target.value })
+  );
+
+  const editButton = createElement(
+    "button",
+    "",
+    '<span class="material-symbols-outlined">edit</span>'
+  );
+  const deleteButton = createElement(
+    "button",
+    "",
+    '<span class="material-symbols-outlined">delete</span>'
+  );
+
+  const editForm = createElement("form");
+  const editInput = createElement("input");
+
+  editInput.value = title;
+
+  editForm.appendChild(editInput);
+
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    updateTask({ id, title: editInput.value, status });
+  });
+
+  editButton.addEventListener("click", () => {
+    tdTitle.innerText = "";
+    tdTitle.appendChild(editForm);
+  });
+
+  editButton.classList.add("btn-action");
+  deleteButton.classList.add("btn-action");
+
+  deleteButton.addEventListener("click", () => {
+    deleteTask(id);
+  });
+
+  tdStatus.appendChild(select);
+
+  tdAction.appendChild(editButton);
+  tdAction.appendChild(deleteButton);
+
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdCreatedAt);
+  tr.appendChild(tdStatus);
+  tr.appendChild(tdAction);
+
+  return tr;
+};
+
+const loadTask = async () => {
+  const tasks = await fetchTasks();
+
+  tbody.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const tr = createRow(task);
+    tbody.appendChild(tr);
+  });
+};
+
+addForm.addEventListener("submit", addTask);
+
+loadTask();
